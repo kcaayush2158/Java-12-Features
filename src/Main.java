295a4e2165860311ca66f2ff613b0653b2268262
Aayush K.C <aayushkc2158@gmail.com>
2019-09-17 10:07:26 +0545

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        
         // you can write a multiple single line of switch case using a lamba expressionion
         String name = "sunday";
         switch (name) {
+            case "monday", "tuesday", "wednesday", "thursday", "friday" -> System.out.println("weekend");
             case "saturday", "sunday" -> System.out.println("weekend");
             default -> System.out.println("invalid");
         }
 
         //you can write a string literal multiple line with the single quote
-
         String html = ` <html>
                             <body>
                                    <p>hello world</p>
